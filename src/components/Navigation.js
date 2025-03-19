c12bb3dbc7f7e0c1690100bc17468971b41982b0
raj232215 <rjsingh232215@gmail.com>
2025-03-19 14:20:05 +0530

    Add src/components/Navigation.js 1742374204472

---
+++ src/components/Navigation.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu as MenuIcon,
+  Map as MapIcon,
+  Campaign as AlarmIcon,
+  Info as InfoIcon,
+  School as EducationIcon,
+  Phone as PhoneIcon,
+} from '@mui/icons-material';
+
+const Navigation = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const menuItems = [
+    { text: 'Route Planner', icon: <MapIcon />, path: '/route-planner' },
+    { text: 'Emergency Alarm', icon: <AlarmIcon />, path: '/emergency-alarm' },
+    { text: 'Information', icon: <InfoIcon />, path: '/information' },
+    { text: 'Educational Tools', icon: <EducationIcon />, path: '/educational' },
+    { text: 'Helpline', icon: <PhoneIcon />, path: '/helpline' },
+  ];
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <List>
+      {menuItems.map((item) => (
+        <ListItem
+          button
+          key={item.text}
+          component={RouterLink}
+          to={item.path}
+          onClick={handleDrawerToggle}
+        >
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.text} />
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Safety App
+          </Typography>
+          {!isMobile && (
+            <div>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  color="inherit"
+                  component={RouterLink}
+                  to={item.path}
+                  startIcon={item.icon}
+                  sx={{ ml: 2 }}
+                >
+                  {item.text}
+                </Button>
+              ))}
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="temporary"
+        anchor="left"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </>
+  );
+};
+
+export default Navigation;
